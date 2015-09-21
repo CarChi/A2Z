f00d7aa202a45989d9a3bf01cebed05c1bca501a
CarolineC <sccarol.chiu@gmail.com>
2015-09-21 16:44:37 -0400

    Update server.js

--- wk1_assignment/server.js
+++ wk1_assignment/server.js
@@ -2,7 +2,7 @@
 var servi = require('servi');
 var app = new servi(true);
 
-// set the port (defaults to 3000 if you leave out this line)
+// set the port
 port(8000);
 
 // serve files from a directory named "public"
