1cee656839ad561ac77c8bcda1a2d7a723785fbe
carolinechiu <ccc521@nyu.edu>
2015-09-29 13:40:44 -0400

    message

---
+++ assignment/wk2/mad lib/server.js
@@ -0,0 +1,19 @@
+// every servi application must have these 2 lines
+var servi = require('servi');
+var app = new servi(true);
+
+// set the port (defaults to 3000 if you leave out this line)
+port(3030);
+
+// serve files from a directory named "public"
+serveFiles("public");
+
+
+route('/',requestHandler);
+
+function requestHandler(request){
+	request.render("index.html");
+}
+
+
+start();
